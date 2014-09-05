6549523f8d7a5100302512667a8174c97d1fa1e2
IvGotAPocket <shibby.exe@gmail.com>
2014-09-05 01:40:28 -0700

    Update GUI_DisplayPanel.java
    -changed
    
    >> removed convert button and now it just automaticlly converts to ascii image
    
    
    >> *implemented a textarea that the converted ascii image outputs to
    
    
    >> other small changes

--- GUI_DisplayPanel.java
+++ GUI_DisplayPanel.java
@@ -1,32 +1,22 @@
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Font;
 import java.awt.image.BufferedImage;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 
 public class GUI_DisplayPanel extends JPanel
 {
-	private final String standard_filepath = "src//imgs//loadimage.png", 
-				            ascii_filepath = "src//imgs//waitimage.png",
-				            arrow_filepath = "src//imgs//arrow.png";
-	
+	private final String standard_filepath = "src//imgs//loadimage.png";
 	private String waitingtoconvert_filepath ="";
 	private JLabel standard_label, ascii_label;
-	private JButton convert_button;
-	private JPanel container_panel; //need reference to use for dialog boxes
+	private JTextArea textarea;
 	
 	
 	//******************************//
@@ -39,16 +29,14 @@ public class GUI_DisplayPanel extends JPanel
 														15,   //left
 														0, 	 //bottom 
 														15)); //right
-		this.container_panel = container_panel;
 		this.displayStandardImage();
-		this.setupConvertButton();
-		this.displayAsciiImage();
+		this.setupTextArea();
 	}
 	
 		
 	
 	//******************************//
-	//** display standard image to covert **//
+	//** setup standard label image - is image to be covert **//
 	//*****************************//
 	private void displayStandardImage() throws IOException
 	{
@@ -57,95 +45,47 @@ public class GUI_DisplayPanel extends JPanel
 		//******************************//
 		
 		this.standard_label.setBorder(BorderFactory.createLoweredBevelBorder());
+		this.standard_label.setSize(400, 400);
 		this.add(this.standard_label, BorderLayout.LINE_START);
 	}
 	
 	
 	
 	//******************************//
-	//** display converted ACSII image **// 
+	//** setup TextArea to display converted image in Ascii art  **// 
 	//*****************************//
-	private void displayAsciiImage() throws IOException
+	private void setupTextArea() throws IOException
 	{	
-		//** load image - set label to imageicon **//
-			this.ascii_label = new JLabel(this.loadImage(ascii_filepath));
-		//******************************//
-		 
-		this.ascii_label.setBorder(BorderFactory.createLoweredBevelBorder());
-		this.add(this.ascii_label, BorderLayout.LINE_END);
+		this.textarea = new JTextArea(100,98);
+		textarea.setFont(new Font("Lucida Console", Font.BOLD, 8));
+		textarea.setBorder(BorderFactory.createLoweredBevelBorder());
+		textarea.setEditable(false);
+		
+		this.add(textarea, BorderLayout.LINE_END);
 	}
 	
 	
 	
 	//******************************//
-	// ** Convert Button ** //
+	// ** Convert Image to Ascii - calls ImageToASCII class code ** //
 	//*****************************//
-	private void setupConvertButton() throws IOException
-	{
-		this.convert_button = new JButton();
-		
-		
-		//** load image -set button to imageicon **//
-			this.convert_button.setIcon(this.loadImage(arrow_filepath));
-		//******************************//
-		
-		
-		//**remove button background**//
-		this.convert_button.setFocusable(false);
-		this.convert_button.setBorder(null); 
-		this.convert_button.setContentAreaFilled(false);
-		//*****************************//
-		
-		
-		//** nested ActionListener **//
-		this.convert_button.addActionListener
-		( 
-			new ActionListener() 
-			{
-				public void actionPerformed(ActionEvent ae) 
-				{
-					if(ae.getSource() == convert_button)
-					{	
-						try{
-							convertImageToAscii();
-						} 
-						catch (IOException e) { e.printStackTrace(); }
-						
-						//changeAsciiIcon(icon); //set image to converted ascii image
-					}
-				}
-			}
-		);
-		//*****************************//
-		
-		this.add(convert_button, BorderLayout.CENTER);
-	}
-	
-	
-	
-	//******************************//
-	// ** convertImage to Ascii - calls ImageToASCII class code ** //
-	//*****************************//
-	private void convertImageToAscii() throws IOException
+	public void convertImageToAscii() throws IOException
 	{
 		ImageToASCII convert = new ImageToASCII( this.getWaitingtoconvert_filepath() );
-		
 		
 		//** -runs convert code on passed in temp image **//
 		//** -returns a 2D array of chars			    **//	
 		char[][] temp = convert.convertImgToAscii(); 
 		//*****************************//
-		
-		
-		this.printToFile(temp);
-		
-		
-		
-		for(int i=0; i < temp.length; i++)
+	
+		//output text to textarea
+		this.textarea.setText(""); //clear textarea
+		for(int i = 0; i < temp.length; i++) 
 		{
-			System.out.println();
-			for(int j=0; j < temp[i].length; j++)
-				System.out.print(temp[i][j]);
+			this.textarea.append("\n");
+			
+		   for(int j = 0; j < temp[i].length; j++) 
+		     this.textarea.append(" " + temp[i][j]);
 		}
 	}
 	
@@ -158,26 +98,6 @@ public class GUI_DisplayPanel extends JPanel
 	{	
 		BufferedImage img = ImageIO.read(new File(filepath)); 
 		return new ImageIcon(img);
-	}
-	
-	
-	
-	//******************************//
-	// ** prints a 2d array out to text file - takes in 2dchar array ** //
-	//*****************************//
-	private void printToFile(char[][] temp) throws IOException
-	{
-		FileWriter write = new FileWriter("temp.txt");
-		BufferedWriter buffer = new BufferedWriter(write);
-		PrintWriter print = new PrintWriter(buffer);
-		
-		for(int i=0; i< temp.length; i++)
-		{
-			print.println();
-			for(int j=0; j < temp[i].length; j++)
-				print.print(temp[i][j]);
-		}
-		print.close();	
 	}
 	
 	
