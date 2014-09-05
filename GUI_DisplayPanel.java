5ca814b159b926cb4ad1d8be4a88c512295971e2
IvGotAPocket <shibby.exe@gmail.com>
2014-09-04 20:21:17 -0700

    Update GUI_DisplayPanel.java
    changed -
    
    
    >> WebCam functionality now working
    
    
    >> Convert button almost working fully
    
    
    >> fixed some bugs
    
    
    >> moved convertImageToAscii to its own class to keep the logic more organized
    
    
    >> added multiple methods to all classes that add logic for different parts of the functionality
    
    
    >> Messed with more functionality for the MenuBar
    
    
    >> Added authors panel for menubar with picture

--- GUI_DisplayPanel.java
+++ GUI_DisplayPanel.java
@@ -2,8 +2,11 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -20,6 +23,7 @@ public class GUI_DisplayPanel extends JPanel
 				            ascii_filepath = "src//imgs//waitimage.png",
 				            arrow_filepath = "src//imgs//arrow.png";
 	
+	private String waitingtoconvert_filepath ="";
 	private JLabel standard_label, ascii_label;
 	private JButton convert_button;
 	private JPanel container_panel; //need reference to use for dialog boxes
@@ -101,23 +105,11 @@ public class GUI_DisplayPanel extends JPanel
 				public void actionPerformed(ActionEvent ae) 
 				{
 					if(ae.getSource() == convert_button)
-					{
-						JOptionPane.showMessageDialog(container_panel,
-											   "Feature coming soon!",
-											    			  "Sorry :(",
-										 JOptionPane.WARNING_MESSAGE);
-						
-						
-						////////////////////////////
-						// CALL CONVERT CODE HERE //
-						
-						
-															//<<<<<<<<<<<<<<<<*********************
-						
-						
-						
-						
-						///////////////////////////
+					{	
+						try{
+							convertImageToAscii();
+						} 
+						catch (IOException e) { e.printStackTrace(); }
 						
 						//changeAsciiIcon(icon); //set image to converted ascii image
 					}
@@ -129,71 +121,63 @@ public class GUI_DisplayPanel extends JPanel
 		this.add(convert_button, BorderLayout.CENTER);
 	}
 	
+	
+	
 	//******************************//
-	//** Loads an image, returns ascii representation **// 
-	//*****************************//	
-	public char[][] convertImgToAscii(String filepath) throws IOException
+	// ** convertImage to Ascii - calls ImageToASCII class code ** //
+	//*****************************//
+	private void convertImageToAscii() throws IOException
 	{
-		final int MAX_SIZE = 50;
-		BufferedImage image = ImageIO.read(new File(filepath));
-
-		// our available characters, is less more? Who knows.
-		String ascii_chars = "@&%#=+:-.  ";
-		// find aspect ratio and orientation for the image.
-		double img_ratio = (double)image.getWidth()/image.getHeight();
-		int num_rows, num_cols;
-		if(img_ratio>1){
-		// image is vertical
-		num_rows = (int)(MAX_SIZE/img_ratio);
-		num_cols = MAX_SIZE;
-		}else{
-		// image is horizontal or square
-		num_rows = MAX_SIZE;
-		num_cols = (int)(MAX_SIZE/img_ratio);
+		ImageToASCII convert = new ImageToASCII( this.getWaitingtoconvert_filepath() );
+		
+		
+		//** -runs convert code on passed in temp image **//
+		//** -returns a 2D array of chars			    **//	
+		char[][] temp = convert.convertImgToAscii(); 
+		//*****************************//
+		
+		
+		this.printToFile(temp);
+		
+		
+		
+		for(int i=0; i < temp.length; i++)
+		{
+			System.out.println();
+			for(int j=0; j < temp[i].length; j++)
+				System.out.print(temp[i][j]);
 		}
-		// Return a 2d array of characters representing the image:
-		char[][] result = new char[num_rows][num_cols];
-
-		// block size is based on aspect ratio
-		int block_w = (int)(image.getWidth()/num_cols);
-		int block_h = (int)(image.getHeight()/num_rows);
-
-		// make an array of ints to hold the pixel values of each block
-		int[] pixels = new int[block_w*block_h];
-
-		// interpret the pixels for each block and find their avg brightness
-		for(int row=0; row<num_rows; row++){
-			for(int col=0; col<num_cols; col++){
-				int block_avg = 0;
-				// fill the pixels array with the values from this block
-				image.getRGB(col*block_w, row*block_h,
-				block_w, block_h, pixels, 0, block_w);
-				for(int rgb:pixels){
-					/*
-					rgb is a 32 bit int representing 4 color channels: alpha/r/g/b
-					We need a sum of the rgb values for each pixel.
-					(rgb>>16)&0xFF) bit shifts the int by 16 places to the right.
-					Then reduces to 1 byte by using & with the bit pattern 11111111.
-					*/
-					block_avg += (((rgb>>16)&0xFF) + ((rgb>>8)&0xFF) + (rgb&0xFF) )/3;
-				}
-				// divide the total by block size to find average block brightness
-				// block_avg could be adjusted up/down to weight the output
-				block_avg /= (block_w*block_h);
-				result[row][col] = ascii_chars.charAt(block_avg/24);
-			}
-		}
-		return result;
 	}
+	
 	
 	
 	//******************************//
 	// ** load image - return ImageIcon ** //
 	//*****************************//
-	public ImageIcon loadImage(String filepath) throws IOException
+	private ImageIcon loadImage(String filepath) throws IOException
 	{	
 		BufferedImage img = ImageIO.read(new File(filepath)); 
 		return new ImageIcon(img);
+	}
+	
+	
+	
+	//******************************//
+	// ** prints a 2d array out to text file - takes in 2dchar array ** //
+	//*****************************//
+	private void printToFile(char[][] temp) throws IOException
+	{
+		FileWriter write = new FileWriter("temp.txt");
+		BufferedWriter buffer = new BufferedWriter(write);
+		PrintWriter print = new PrintWriter(buffer);
+		
+		for(int i=0; i< temp.length; i++)
+		{
+			print.println();
+			for(int j=0; j < temp[i].length; j++)
+				print.print(temp[i][j]);
+		}
+		print.close();	
 	}
 	
 	
@@ -209,6 +193,21 @@ public class GUI_DisplayPanel extends JPanel
 	public void changeAsciiIcon(ImageIcon icon)
 	{
 		ascii_label.setIcon(icon);
+	}
+	
+	
+	
+	//*****************************//
+	//** temp_waittoconvert_filepath - setter/getter - changes filepath to image to be converted **//
+	//*****************************//
+	public String getWaitingtoconvert_filepath()
+	{
+		return waitingtoconvert_filepath;
+	}
+
+	public void setWaitingtoconvert_filepath(String waitingtoconvert_filepath)
+	{
+		this.waitingtoconvert_filepath = waitingtoconvert_filepath;
 	}
 	
 }
