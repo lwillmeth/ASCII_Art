8898107f1ccf01864ac62b017b75fba89ee14669
IvGotAPocket <shibby.exe@gmail.com>
2014-09-06 16:58:57 -0700

    Update GUI_DisplayPanel.java
    >> mess with font for textarea --still needs work
    
    >> changed some variable names so they made more sense
    
    >> fixed a bug by adding a condition to my getter method

--- GUI_DisplayPanel.java
+++ GUI_DisplayPanel.java
@@ -9,15 +9,15 @@ import javax.swing.JTextArea;
 
 public class GUI_DisplayPanel extends JPanel
 {
-	private final String standard_filepath = "src//imgs//loadimage.png";
-	private String waitingtoconvert_filepath ="";
+	private final String loadimage_filepath = "src//imgs//loadimage.png";
+	private String imagetoconvert_filepath = "";
 	private JLabel standard_label, ascii_label;
 	private JTextArea textarea;
 	
 	//******************************//
 	// ** Constructor ** //
 	//*****************************//
-	public GUI_DisplayPanel(JPanel container_panel) throws IOException
+	public GUI_DisplayPanel() throws IOException
 	{	
 		this.setLayout(new BorderLayout());
 		this.setBorder(BorderFactory.createEmptyBorder( 0,	 //top
@@ -36,7 +36,7 @@ public class GUI_DisplayPanel extends JPanel
 	private void displayStandardImage() throws IOException
 	{
 		//** load image - set label to imageicon **//
-    	this.standard_label = new JLabel(GUI_Main.loadImage(standard_filepath));
+    	this.standard_label = new JLabel(GUI_Main.loadImage(loadimage_filepath));
 		this.standard_label.setBorder(BorderFactory.createLoweredBevelBorder());
 		this.standard_label.setSize(400, 400);
 		this.add(this.standard_label, BorderLayout.LINE_START);
@@ -50,7 +50,7 @@ public class GUI_DisplayPanel extends JPanel
 	private void setupTextArea() throws IOException
 	{	
 		this.textarea = new JTextArea(100,98);
-		textarea.setFont(new Font("Lucida Console", Font.BOLD, 8));
+		textarea.setFont(new Font("Courier", Font.BOLD, 8));
 		textarea.setBorder(BorderFactory.createLoweredBevelBorder());
 		textarea.setEditable(false);
 		
@@ -64,7 +64,7 @@ public class GUI_DisplayPanel extends JPanel
 	//*****************************//
 	public void convertImageToAscii() throws IOException
 	{
-		ImageToASCII convert = new ImageToASCII( this.getWaitingtoconvert_filepath() );
+		ImageToASCII convert = new ImageToASCII( this.getImageToConvert_filepath() );
 		
 		//** -runs convert code on passed in temp image **//
 		//** -returns a 2D array of chars			    **//	
@@ -98,16 +98,19 @@ public class GUI_DisplayPanel extends JPanel
 	
 	
 	//*****************************//
-	//** temp_waittoconvert_filepath - setter/getter - changes filepath to image to be converted **//
+	//** ImageToConvert_filepath - setter/getter - changes filepath to image to be converted **//
 	//*****************************//
-	public String getWaitingtoconvert_filepath()
+	public String getImageToConvert_filepath()
 	{
-		return waitingtoconvert_filepath;
+		if(this.imagetoconvert_filepath == null)
+			return this.loadimage_filepath;
+		else
+			return imagetoconvert_filepath;
 	}
 
-	public void setWaitingtoconvert_filepath(String waitingtoconvert_filepath)
+	public void setImageToConvert_filepath(String waitingtoconvert_filepath)
 	{
-		this.waitingtoconvert_filepath = waitingtoconvert_filepath;
+		this.imagetoconvert_filepath = waitingtoconvert_filepath;
 	}
 	
 }
